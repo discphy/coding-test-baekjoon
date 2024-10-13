2176ac75fce69970a80dd0e3ccf1698f4f585bef
discphy <dinophy@nate.com>
2024-10-13 16:22:02 +0900

    11399

---
+++ src/algorithm/greedy/_11399.java
@@ -0,0 +1,31 @@
+package algorithm.greedy;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _11399 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner kb = new Scanner(System.in);
+        int answer = 0;
+
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            answer += sum;
+        }
+
+        System.out.println(answer);
+    }
+}
